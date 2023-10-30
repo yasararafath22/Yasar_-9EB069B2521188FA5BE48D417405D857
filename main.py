e485bb8540e0bcc82ee0b8b8d19883614ebf7cb2
yasararafath22 <144357170+yasararafath22@users.noreply.github.com>
2023-10-30 16:31:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-def linearSearchProduct(productList, targetProduct):
-  indices = []
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__balance = initial_balance
 
-  for index, product in enumerate(productList):
-    if product == targetProduct:
-      indices.append(index)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            return f"Deposited ${amount}. New balance: ${self.__balance}"
+        else:
+            return "Invalid deposit amount"
 
-  return indices
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.__balance}"
+        elif amount > self.__balance:
+            return "Insufficient funds"
+        else:
+            return "Invalid withdrawal amount"
 
+    def display_balance(self):
+        return f"Account Balance for {self.__account_holder_name}: ${self.__balance}"
 
 # Example usage:
-products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
-target = "shoes"
-target2 = 'apple'
-result = linearSearchProduct(products, target)
-print(result)
+if __name__ == "__main__":
+    # Create a new bank account
+    account = BankAccount("12345", "John Doe", 1000.0)
+
+    # Check the initial balance
+    print(account.display_balance())
+
+    # Deposit money
+    print(account.deposit(500))
+
+    # Withdraw money
+    print(account.withdraw(200))
+
+    # Check the updated balance
+    print(account.display_balance())
